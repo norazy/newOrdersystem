612c26e9766ff65d8abdf8bf91e8f8809c27cf61
EC2 Default User <ec2-user@ip-172-31-18-222.ap-southeast-1.compute.internal>
2019-02-07 08:20:43 +0000

    finish preorder

--- app/assets/javascripts/orderlist.js
+++ app/assets/javascripts/orderlist.js
@@ -15,7 +15,6 @@ window.addEventListener('turbolinks:load', function() {
   }); 
 });
 
-// var box
 // モーダルの呼び出し
 $(document).on('turbolinks:load', function() {
   $(".menu_indiv_block").click(function(event){
@@ -25,30 +24,29 @@ $(document).on('turbolinks:load', function() {
     $.ajax({
         url: url,
         success: function(modal){
+          // ↑これで呼び出すとhtmlのheadとかも全部呼び出される
+          // ↓なのでappendしたいidのみ呼び出す
           modal_bg = $(modal).filter("#modal_bg");
           modal_menu = $(modal).filter("#modal_menu_indiv");
           modal_close = $(modal).filter("#modal_close");
-          
+
           $("body").append(modal_bg);
           $("body").append(modal_menu);
           $("body").append(modal_close);
 
-          // console.log(modal_bg);
-          // box = $.parseHTML(modal)
-          // $("body").append(modal);
-
-          // モーダルを消す。ボタンの動き
+           // モーダルを消す。ボタンの動き
            $("#modal_close").click(function(){
-                // console.log(1)
                 $("#modal_bg").remove();
                 $("#modal_menu_indiv").remove();
             });
+          
           // ↓注文個数のクリック後の色変更
           $(".modal_menu_number label").click(function(){
             $(".modal_menu_number").css("background-color", "#f5f5dc");
             $(this).parent().css("background-color", "#ea5317");
           });
-          // ↓オプションの選択部分
+          
+          // ↓オプションの選択部分、選択すると色が変わる
           $(".modal_menu_option label").click(function(){
             $(".modal_menu_option").css("background-color", "#000a34");
             $(this).parent().css("background-color", "#ea5317");
@@ -56,42 +54,64 @@ $(document).on('turbolinks:load', function() {
         }
     });
   });
-  
 })  
 
+// 未確定メニューの数量変更　マイナス
+$(document).on('turbolinks:load', function() {
+  $(".minus_btn").click(function(){
+    
+    var number = $(".minus_btn").index(this);
+    var letter = $(".preorder_menu_number2 p").eq(number).html();
+    // 文字列を数値にする↓
+    letter = parseInt(letter);
 
-// モーダルを消す。ボタンの動き
-// $(document).on('turbolinks:load', function() {
-// $(function() {
-// window.addEventListener('turbolinks:load', function() {
+    if (letter >= 1) {
+        var letter2 = letter - 1
+        $(".preorder_menu_number2 p").eq(number).html(letter2);
+        $(".orderlist_number_value").eq(number).val(letter2);
+    }
+  });
+})
 
-//     $("#modal_close").click(function(){
-//       console.log(1)
-//       // $(this).remove();
-//       $("#modal_bg").remove();
-//       $("#modal_menu_indiv").remove();
-//       // $.parseHTML(box);
+// 未確定メニューの数量変更　プラス
+$(document).on('turbolinks:load', function() {
+  $(".plus_btn").click(function(){
+    var number = $(".plus_btn").index(this);
+    var letter = $(".preorder_menu_number2 p").eq(number).html();
+    // 文字列を数値にする↓
+    letter = parseInt(letter);
 
-//       // $("body").remove(box);
-//       // console.log(box);
-//       // $(box).remove();
-//     });
-// })
-// ↑これに「$(document).on('turbolinks:load', function() {」をつけると
-// 逆にボタンが動かなくなる
+    if (letter < 4 ) {
+        var letter2 = letter + 1
+        $(".preorder_menu_number2 p").eq(number).html(letter2);
+        $(".orderlist_number_value").eq(number).val(letter2);
 
-// // ↓注文個数のクリック後の色変更
-// $(function() {
-//   $(".modal_menu_number label").click(function(){
-//     $(".modal_menu_number").css("background-color", "#f5f5dc");
-//     $(this).parent().css("background-color", "#ea5317");
-//   });
-// })
+    }
+  });
+})
 
-// // ↓オプションの選択部分
-// $(function() {
-//   $(".modal_menu_option label").click(function(){
-//     $(".modal_menu_option").css("background-color", "#000a34");
-//     $(this).parent().css("background-color", "#ea5317");
-//   });
-// })
+// 合計を求める
+$(document).on('turbolinks:load', function() {
+  var sum = 0
+  // 合計を入れる変数を作成
+  
+  $(".ordered_menu").each(function(i){
+    // each文で注文済みのメニューを全部繰り返す
+    
+    var price = $(".ordered_menu_price p").eq(i).html();
+    // 価格を取り出す
+    var number = $(".ordered_menu_number p").eq(i).html();
+    // 個数を取り出す
+    var subtotal = price * number
+    // 各オーダーの小計を出す
+
+    sum += subtotal
+    // 小計をこれまでの合計に足す
+  });
+  
+  var sum2 = String(sum).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+  // 合計の数値を金額表示にする
+
+  $("#ordered_sum_price p").html(sum2);
+  // 合計の部分を表示させる
+})
--- app/controllers/orderlist_controller.rb
+++ app/controllers/orderlist_controller.rb
@@ -1,7 +1,10 @@
 class OrderlistController < ApplicationController
     # ↓サインインしているかどうかの確認
     # before_action :move_to_signin 
-    
+    # def test
+    #     render json: Orderlist.where(user_id: current_user.id)
+    # end
+
     def top_page
     end
 
@@ -9,12 +12,14 @@ class OrderlistController < ApplicationController
         @menus1 = Menu.where(category: 11)
         @menus2 = Menu.where(category: 12)
         @menus3 = Menu.where(category: 13)
+        # binding.pry
     end
 
     def alacarte
         @menus1 = Menu.where(category: 21)
         @menus2 = Menu.where(category: 22)
         @menus3 = Menu.where(category: 23)
+        # binding.pry
     end
 
     def noodle
@@ -42,6 +47,7 @@ class OrderlistController < ApplicationController
         @menus1 = Menu.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
     end
     
+    # おすすめ商品
     def recommend
         # レコメンドカテゴリーごと取り出す
         category_reco1 = Recommend.where(category_reco: 1)
@@ -64,7 +70,7 @@ class OrderlistController < ApplicationController
             @menus3 << Menu.find(reco[:menu_id])
         end
     end
-    
+
     # モーダル部分
     def modal
         # パラムズのidにあったメニューを取り出す
@@ -93,10 +99,8 @@ class OrderlistController < ApplicationController
             format.js
         end
     end
-
-    # 「カートへ」のcreate
+    # モーダル「カートへ」のcreate
     def order_create
-        # binding.pry
         # メニュー価格の呼び出し
         menu = Menu.find(orderlist_params[:menu_id])
         menu_price = menu.price
@@ -109,16 +113,148 @@ class OrderlistController < ApplicationController
             option_price = option.price_opt
 
             Orderlist.create(user_id: current_user.id, menu_id: orderlist_params[:menu_id], price: menu_price, number: orderlist_params[:number], state: "0")
-            Orderlist.create(user_id: current_user.id, menu_id: orderlist_params[:menu_id], option_id: orderlist_params[:option_id], price: option_price, number: orderlist_params[:number], state: "0")
+            # ↓ここでオプションにメニューidを持たせたないのは
+            # 未確定のところで、メニューidを持ってるかどうかで条件分岐させているから
+            # （オプションidで条件分岐させてもよかったんだけど。。）
+            Orderlist.create(user_id: current_user.id, option_id: orderlist_params[:option_id], price: option_price, number: orderlist_params[:number], state: "0")
         else
             Orderlist.create(user_id: current_user.id, menu_id: orderlist_params[:menu_id], price: menu_price, number: orderlist_params[:number], state: "0")
         end
         
         # createのあとは新しいビューに飛ばずに、backするように設定する
+        # かつ、「カートに入れた」っていうメッセージを出す
         redirect_back(fallback_location: root_path)
-        # flash.now[:notice] = "カートにいれました！"
+        # flash[:notice] = "カートにいれました！"
     end
 
+    # 未確定注文のページ
+    def pre_order
+        # 現在ログインしているユーザーの状態が０のオーダーを引き出す
+        preorder = Orderlist.where(user_id: params[:id]).where(state: 0)
+        
+        # ビューに渡す配列作成
+        @preorder = []
+        
+        # 取り出したオーダーに対して、一つずつハッシュを作る
+        # {:order_id, :number, :state, :menu_name, :option_name}　←キー
+        # orderlistのテーブルにmenu_idはあるけど、
+        # メニュー名のカラムがないからハッシュを作ってる
+        preorder.each do |order|
+            hash = {}
+            hash[:order_id] = order.id
+            hash[:number] = order.number
+            hash[:state] = 1
+            # viewに表示させるために必要↓
+            if order.menu_id then
+                number = order.menu_id
+                menu = Menu.find(number)
+                name = menu.name
+                hash[:menu_name] = name
+            else
+                number2 = order.option_id
+                option = Optiontable.find(number2)
+                name2 = option.name_opt
+                hash[:option_name] = name2
+            end
+            @preorder << hash
+        end
+        @orderlist = Orderlist.new
+    end
+    
+    # 未確定のものを確定にする部分
+    def post_order
+        # paramsの配列を変数へ
+        preorderlist = params[:orderlist]
+        
+        # 配列をeach文にかける
+        preorderlist.each do |pre|
+            
+            # ↓オーダーのidを取り出す
+            number = pre[":id"]
+            # ↓文字列になっているため、数字にする
+            number = number.to_i
+            # ↓該当するidのオーダーレコードをだす
+            orderlist = Orderlist.find(number)
+
+            # ↓useridの確認
+            if orderlist.user_id = current_user.id then
+                # 個数の書き換え
+                orderlist.number = pre[":number"]
+                # 状態の書き換え
+                orderlist.state = pre[":state"]
+                # 上書き保存
+                orderlist.save
+            end
+        end
+
+        # 自分が最初に作ったパラムズに対してのコード
+        # 取り出したparamsは{"id"=>["17", "18"....], "number"=>[], "state"=>[]}
+        # # paramsがidごとに配列になっているので、それぞれをパラムズから取り出す
+        # id = params[:orderlist][:id]
+        # number = params[:orderlist][:number]
+        # state = params[:orderlist][:state]
+        
+        # # 入っている個数を数える
+        # length = id.length
+       
+        # # while文で一つずつ取り出して保存する
+        # i = 0
+        # while i <= length-1 do 
+        #     order_id = id[i]
+        #     order_number = number[i]
+        #     order_state = state[i]
+            
+        #     orderlist = Orderlist.find(order_id)
+
+        #     if orderlist.user_id = current_user.id then
+        #         orderlist.number = order_number
+        #         orderlist.state = order_state
+        #         orderlist.save
+        #     end
+        # i = i + 1
+
+        # end
+        redirect_to root_path
+    end
+
+    # 注文済画面
+    def ordered
+        # 現在ログインしているユーザーの状態が０のオーダーを引き出す
+        ordered = Orderlist.where(user_id: params[:id]).where(state: 1..3).order('state ASC')
+        # binding.pry
+        # ビューに渡す配列作成
+        @ordered = []
+        
+        # 取り出したオーダーに対して、一つずつハッシュを作る
+        # {:order_id, :number, :state, :menu_name, :option_name}
+        ordered.each do |order|
+            hash = {}
+            hash[:number] = order.number
+            hash[:price] = order.price
+            if order.menu_id then
+                number = order.menu_id
+                menu = Menu.find(number)
+                name = menu.name
+                hash[:menu_name] = name
+            else
+                number2 = order.option_id
+                option = Optiontable.find(number2)
+                name2 = option.name_opt
+                hash[:option_name] = name2
+            end
+            
+            if order.state == 1 then
+                hash[:state] = "注文済"
+            elsif order.state == 2 then
+                hash[:state] = "調理中"
+            else
+                hash[:state] = "届済み"
+            end    
+            
+            @ordered << hash
+        end
+
+    end
 
     private
     def orderlist_params
@@ -129,5 +265,11 @@ class OrderlistController < ApplicationController
     def move_to_signin
         redirect_to new_user_session_url unless user_signed_in?
     end
+
+    # def post_order_params
+    #     # params.require(:orderlist).permit
+    #     params.require(:orderlist).map{|o| o.permit([:order_id, :number, :state]) }
+    # end
+
 
 end
